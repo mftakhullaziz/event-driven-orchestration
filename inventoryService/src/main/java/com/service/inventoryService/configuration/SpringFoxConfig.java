9ad5421196e7d70cad1a312d7ab8f7ea36978120
amifth <miftakhul.14116078@student.itera.ac.id>
2023-01-14 10:38:06 +0700

    added

--- inventoryService/src/main/java/com/service/inventoryService/configuration/SpringFoxConfig.java
+++ inventoryService/src/main/java/com/service/inventoryService/configuration/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.service.inventoryService.config;
+package com.service.inventoryService.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- orchestratorDomainService/src/main/java/com/service/orchestratorDomainService/OrchestratorDomainServiceApplication.java
+++ orchestratorDomainService/src/main/java/com/service/orchestratorDomainService/OrchestratorDomainServiceApplication.java
@@ -2,8 +2,10 @@ package com.service.orchestratorDomainService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrchestratorDomainServiceApplication {
 
 	public static void main(String[] args) {
